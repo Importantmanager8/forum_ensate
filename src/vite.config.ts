c8a038a263c9bb6f5c496397857de918a06c006e
Marwane El Khoukh <167308566+Importantmanager8@users.noreply.github.com>
2025-08-24 00:58:36 +0100

    some edits on vite and package json

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -2,10 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 8080, // same port as your start command
-    host: '0.0.0.0',
-    allowedHosts: ['glorious-upliftment-production-3c02.up.railway.app']
-  }
+    plugins: [react()],
 })
