b2ad436c9cf464ceef8c6ed818145303c8d8871b
Marwane El Khoukh <167308566+Importantmanager8@users.noreply.github.com>
2025-08-24 01:01:02 +0100

    some changes on vite

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-})
+  server: {
+    proxy: {
+      port: 8080, // same port as your start command
+      host: '0.0.0.0',
+      allowedHosts: ['glorious-upliftment-production-3c02.up.railway.app']
+    },
+  },
+  plugins: [react()],
+  // Remove the optimizeDeps.exclude for lucide-react to fix module loading
+});
